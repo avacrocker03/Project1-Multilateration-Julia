e27e492b4d710be73bc41a7595b4ea32c458ae7f
avacrocker03 <118841961+avacrocker03@users.noreply.github.com>
2023-08-24 11:19:32 -0400

    Create mult.jl

---
+++ mult.jl
@@ -0,0 +1,12 @@
+#=
+ = Author: Ava Crocker, acrocker2021@,my.fit.edu
+ = Author: Taylor Carlson, tcarlson2021@my.fit.edu
+ = Course: CSE 4250, Fall 2023
+ = Project: 
+ = Implementation:
+=#
+
+satellites = Dict()
+print("Enter satellite locations: ")
+for i in range
+    readline()
