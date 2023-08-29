799846cc89d97ef513b3da4159a33443e5f55aa6
avacrocker03 <118841961+avacrocker03@users.noreply.github.com>
2023-08-29 12:35:44 -0400

    Update mult.jl

--- mult.jl
+++ mult.jl
@@ -7,12 +7,50 @@
 =#
 
 function strToFloat(dataStr)
+    # converting string input to float
     floatData = []
-    for str in dataStr
-        str = parse(Float64, str)
-        push!(floatData, str)
+    for str in dataStr # iterating through input 
+        str = parse(Float64, str) # parsing data to float
+        push!(floatData, str) # adding to new array of floats
     end
     return floatData
+end
+
+function distance(time)
+    # calculating the distance from the satellite to the target (ie. Ri,Rj,Rk,Rl)
+    c = 299792458 # speed of light (seconds)
+    r = c*time
+    return r
+end
+
+function coordDiff(coord1, coord2)
+    # calculating difference of satellite coordinates (ie. xij, yjk, etc.)
+    coordD = coord1 - coord2
+    return coordD
+end
+
+function rDiff(r1,r2)
+    # calculating difference of R values (ie. Rij, Rjk, etc.)
+    rd = r1 - r2
+    return rd
+end
+
+function xCombo(r1,r2,r3,r4,c1,c2,c3,c4)
+    # calculating the difference of the products of R*y values (ie. Xijy, Xikx, etc.)
+    xC = (rDiff(r1,r2)*coordDiff(c1,c2)) - (rDiff(r3,r4)*coordDiff(c3,c4))
+    return xC
+end
+
+function sEqn(x,y,z)
+    # calculating product of coords squared (ie. Si2, Sj2, etc.)
+    s = x^2 + y^2 + z^2
+    return s
+end
+
+function rcCombo(r1,r2,x1,x2,y1,y2,z1,z2)
+    # calculating the sum of R diff values squared and the difference of S values (ie. Rij2xyz, Rik2xyz, etc.)
+    rxyz = (rDiff(r1,r2)^2) + sEqn(x1,y1,z1) - sEqn(x2,y2,z2)
+    return rxyz
 end
 
 
@@ -40,24 +78,22 @@ function main()
     end
     times = []
 
-    for i in range(0,1)
+    for i in range()
         time = readline()
         if (i == 0)
             timeStr = split(time, " ")
-            push!(times, strToFloat(timeStr))
+            push!(times, (strToFloat(timeStr)*10^-9))
         end
         if (i == 1)
             timeStr1 = split(time, " ")
-            push!(times, strToFloat(timeStr1))
+            push!(times, (strToFloat(timeStr1)*10^-9))
         end
     end
 
-    println()
-    println(times)
-    println()
-    println("g= ,h= ,j= ,m= ,o= ")
-    println("+) x= , y= , z= ")
-    println("-) x= , y= , z= ")
+    ri = distance(times[1][1])
+    rj = distance(times[1][2])
+    rk = distance(times[1][3])
+    rl = distance(times[1][4])
 
     xi = satellites[1][1]
     yi = satellites[1][2]
@@ -68,18 +104,32 @@ function main()
     xl = satellites[4][1]
     yl = satellites[4][2]
 
-    xji = satellites[2][1] - satellites[1][1]
-    xki = satellites[3][1] - satellites[1][1]
-    xjk = satellites[2][1] - satellites[3][1]
-    xlk = satellites[4][1] - satellites[3][1]
-    yki = satellites[3][2] - satellites[1][2]
-    yji = satellites[2][2] - satellites[1][2]
-    ylk = satellites[4][2] - satellites[3][2]
-    yjk = satellites[2][2] - satellites[3][2]
-    zji = satellites[2][3] - satellites[1][3]
-    zki = satellites[3][3] - satellites[1][3]
-    zjk = satellites[2][3] - satellites[3][3]
-    zlk = satellites[4][3] - satellites[3][3]
+    a = xCombo(ri,rk,ri,rj,xj,xi,xk,xi)/xCombo(ri,rj,ri,rk,yk,yi,yj,yi) # XijyA = Xikx
+    println(a)
+
+
+    # xji = satellites[2][1] - satellites[1][1]
+    # xki = satellites[3][1] - satellites[1][1]
+    # xjk = satellites[2][1] - satellites[3][1]
+    # xlk = satellites[4][1] - satellites[3][1]
+    # yki = satellites[3][2] - satellites[1][2]
+    # yji = satellites[2][2] - satellites[1][2]
+    # ylk = satellites[4][2] - satellites[3][2]
+    # yjk = satellites[2][2] - satellites[3][2]
+    # zji = satellites[2][3] - satellites[1][3]
+    # zki = satellites[3][3] - satellites[1][3]
+    # zjk = satellites[2][3] - satellites[3][3]
+    # zlk = satellites[4][3] - satellites[3][3]
+
+
+    println()
+    println(satellites)
+    println()
+    println(times)
+    println()
+    println("g= ,h= ,j= ,m= ,o= ")
+    println("+) x= , y= , z= ")
+    println("-) x= , y= , z= ")
 end
 
 main()
